8f674d7a859f821d6a705891509926dfadd94ede
NingthemSapam <143411524+NingthemSapam@users.noreply.github.com>
2025-02-11 02:00:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+  const showbar = document.querySelector('.divSidebar');
+  showbar.style.display = "flex";
+}
+
+function hideSidebar(){
+    const hidebar = document.querySelector('.divSidebar');
+    hidebar.style.display = "none";
+}
